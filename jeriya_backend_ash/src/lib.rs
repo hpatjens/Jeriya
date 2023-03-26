eb941a908e723e42c8515a1a3d44877dc4107f01
Henrik Patjens <hpatjens@gmail.com>
2023-03-26 18:50:02 +0200

    rewrote create_instance and changed error handling

--- jeriya_backend_ash/src/lib.rs
+++ jeriya_backend_ash/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ffi::CString;
+use std::ffi::{CStr, CString, NulError};
 
 use jeriya::Backend;
 
@@ -7,19 +7,42 @@ use ash::{
         ext::DebugUtils,
         khr::{self},
     },
-    vk::{self, Result},
+    prelude::VkResult,
+    vk::{self},
     Entry, Instance, LoadingError,
 };
-use jeriya_shared::{log::info, winit::window::Window, Error};
-use thiserror::Error;
+use jeriya_shared::{log::info, winit::window::Window};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+trait IntoJeriya {
+    type Output;
+    fn into_jeriya(self) -> Self::Output;
+}
+
+impl<T> IntoJeriya for VkResult<T> {
+    type Output = Result<T>;
+
+    fn into_jeriya(self) -> Self::Output {
+        self.map_err(|err| Error::Result(err))
+    }
+}
 
 /// Errors in the ash backend
-#[derive(Error, Debug)]
-pub enum AshError {
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
     #[error("Error while loading Vulkan {:?}", .0)]
-    LoadingError(LoadingError),
+    LoadingError(#[from] LoadingError),
     #[error("Error while executing a Vulkan operation {:?}", .0)]
-    Result(Result),
+    Result(#[from] vk::Result),
+    #[error("Error while converting a string: {:?}", .0)]
+    StringNulError(#[from] NulError),
+}
+
+impl From<Error> for jeriya_shared::Error {
+    fn from(value: Error) -> Self {
+        jeriya_shared::Error::Backend(Box::new(value))
+    }
 }
 
 pub struct Ash {
@@ -32,44 +55,74 @@ impl Backend for Ash {
         Self: Sized,
     {
         if windows.is_empty() {
-            return Err(Error::ExpectedWindow);
+            return Err(jeriya_shared::Error::ExpectedWindow);
         }
 
-        let entry = unsafe { Entry::load().map_err(|err| Error::Backend(Box::new(err)))? };
+        info!("Creating Vulkan Entry");
+        let entry = unsafe { Entry::load().map_err(Error::LoadingError)? };
+
+        info!("Creating Vulkan Instance");
         let application_name = application_name.unwrap_or(env!("CARGO_PKG_NAME"));
-        let instance = create_instance(&entry, application_name);
+        let instance = create_instance(&entry, application_name)?;
 
         Ok(Self { instance })
     }
 }
 
-pub fn create_instance(entry: &Entry, application_name: &str) -> Instance {
-    let application_name = CString::new(application_name).unwrap();
+pub fn list_strings<'a>(strings: impl IntoIterator<Item = impl AsRef<str>>) -> String {
+    strings
+        .into_iter()
+        .map(|s| format!("\t{}", s.as_ref()))
+        .collect::<Vec<_>>()
+        .join("\n")
+}
 
-    let available_layers = entry
+fn available_layers(entry: &Entry) -> Vec<String> {
+    entry
         .enumerate_instance_layer_properties()
         .unwrap()
         .iter()
         .map(|properties| &properties.layer_name)
         .map(|array| jeriya_shared::c_null_terminated_char_array_to_string(array).unwrap())
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
 
-    let available_layers_string = available_layers.iter().map(|s| format!("\t{}", s)).collect::<Vec<_>>().join("\n");
-    info!("Available Layers:\n{}", available_layers_string);
+pub fn create_instance(entry: &Entry, application_name: &str) -> Result<Instance> {
+    let application_name = CString::new(application_name).unwrap();
 
+    // Available Layers
+    let available_layers = available_layers(entry);
+    info!("Available Layers:\n{}", list_strings(&available_layers));
+
+    // Active Layers
     let layer_names = available_layers
-        .iter()
+        .into_iter()
         .filter(|layer| layer == &"VK_LAYER_LUNARG_standard_validation" || layer == &"VK_LAYER_KHRONOS_validation")
-        .map(|layer| CString::new(layer.as_str()).unwrap())
         .collect::<Vec<_>>();
+    info!("Active Layers:\n{}", list_strings(&layer_names));
 
-    let layers_names_raw: Vec<*const i8> = layer_names.iter().map(|raw_name| raw_name.as_ptr()).collect();
-
-    let extension_names_raw = vec![
-        khr::Surface::name().as_ptr(),
-        khr::Win32Surface::name().as_ptr(),
-        DebugUtils::name().as_ptr(),
+    // Active Extensions
+    fn expect_extension(extension_name: &'static CStr) -> &str {
+        extension_name.to_str().expect("failed to converte extension name")
+    }
+    let extension_names = vec![
+        expect_extension(khr::Surface::name()),
+        expect_extension(khr::Win32Surface::name()),
+        expect_extension(DebugUtils::name()),
     ];
+    info!("Active Extensions:\n{}", list_strings(&extension_names));
+
+    let layer_names = layer_names
+        .into_iter()
+        .map(|layer| CString::new(layer.as_str()).map_err(|err| Error::StringNulError(err)))
+        .collect::<Result<Vec<_>>>()?;
+    let extension_names = extension_names
+        .iter()
+        .map(|s| CString::new(*s).map_err(|err| Error::StringNulError(err)))
+        .collect::<Result<Vec<_>>>()?;
+
+    let layers_names_ptrs: Vec<*const i8> = layer_names.iter().map(|raw_name| raw_name.as_ptr()).collect();
+    let extension_names_ptrs: Vec<*const i8> = extension_names.iter().map(|raw_name| raw_name.as_ptr()).collect();
 
     let appinfo = vk::ApplicationInfo::builder()
         .application_name(&application_name)
@@ -80,10 +133,10 @@ pub fn create_instance(entry: &Entry, application_name: &str) -> Instance {
 
     let create_info = vk::InstanceCreateInfo::builder()
         .application_info(&appinfo)
-        .enabled_layer_names(&layers_names_raw)
-        .enabled_extension_names(&extension_names_raw);
+        .enabled_layer_names(&layers_names_ptrs)
+        .enabled_extension_names(&extension_names_ptrs);
 
-    unsafe { entry.create_instance(&create_info, None).expect("Instance creation error") }
+    unsafe { entry.create_instance(&create_info, None).into_jeriya() }
 }
 
 #[cfg(test)]
@@ -110,7 +163,7 @@ mod tests {
 
         #[test]
         fn empty_windows_none() {
-            assert!(matches!(Ash::new(None, &[]), Err(Error::ExpectedWindow)));
+            assert!(matches!(Ash::new(None, &[]), Err(jeriya_shared::Error::ExpectedWindow)));
         }
     }
 }
