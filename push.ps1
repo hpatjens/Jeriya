c0c7a98a66b0520ed0469aa1e33634a77a7d1923
Henrik Patjens <hpatjens@gmail.com>
2023-03-31 17:09:49 +0200

    push script

--- push.ps1
+++ push.ps1
@@ -1,3 +1,7 @@
+git push
+
+cargo nextext run
+
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -Outfile codecov.exe
 cargo llvm-cov --locked --all-features --lcov --output-path lcov.info
