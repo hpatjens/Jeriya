ca2652708192beeb06fa435cd69964348811d444
Henrik Patjens <hpatjens@gmail.com>
2023-03-31 19:17:23 +0200

    updated run_jobs

--- run_jobs.ps1
+++ run_jobs.ps1
@@ -2,6 +2,14 @@ cargo nextest run
 
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -Outfile codecov.exe
+
 cargo llvm-cov nextest --locked --all-features --lcov --output-path lcov.info
+
 .\codecov.exe -t ${CODECOV_TOKEN}
-git rev-parse HEAD
+if ($?) {
+    Write-Host "Codecov Successful" -ForegroundColor Green
+} else {
+    Write-Host "Codecov Failed" -ForegroundColor Red
+}
+
+Write-Host "Git Commit: $(git rev-parse HEAD)"  -ForegroundColor Cyan
