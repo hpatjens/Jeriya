3778c4e0572f87a4313a1c76438fb39f12e8270b
Henrik Patjens <hpatjens@gmail.com>
2023-03-31 17:04:19 +0200

    added cargo llvm-cov to script

--- codecov.ps1
+++ codecov.ps1
@@ -1,3 +1,4 @@
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -Outfile codecov.exe
+cargo llvm-cov --locked --all-features --lcov --output-path lcov.info
 .\codecov.exe -t ${CODECOV_TOKEN}
