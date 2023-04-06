f7f487d5b578ec87c7831185fe215f02d881e13c
Henrik Patjens <hpatjens@gmail.com>
2023-04-06 21:06:20 +0200

    added CommandPool

---
+++ jeriya_backend_ash_core/src/command_pool.rs
@@ -0,0 +1,76 @@
+use std::{rc::Rc, sync::Arc};
+
+use ash::vk;
+
+use crate::{device::Device, queue::Queue, AsRawVulkan};
+
+pub struct CommandPool {
+    command_pool: vk::CommandPool,
+    device: Arc<Device>,
+}
+
+impl CommandPool {
+    /// Creates a new `CommandPool` for the given `queue_family_index`
+    pub unsafe fn new_from_family(
+        device: &Arc<Device>,
+        queue_family_index: u32,
+        command_pool_create_flags: vk::CommandPoolCreateFlags,
+    ) -> crate::Result<Rc<Self>> {
+        let command_pool_create_info = vk::CommandPoolCreateInfo::builder()
+            .flags(command_pool_create_flags)
+            .queue_family_index(queue_family_index);
+        let command_pool = device.as_raw_vulkan().create_command_pool(&command_pool_create_info, None)?;
+        Ok(Rc::new(Self {
+            device: device.clone(),
+            command_pool,
+        }))
+    }
+
+    /// Create a new `CommandPool` for the given `queue`.
+    pub fn new(device: &Arc<Device>, queue: &Queue, command_pool_create_flags: vk::CommandPoolCreateFlags) -> crate::Result<Rc<Self>> {
+        unsafe { Self::new_from_family(device, queue.queue_family_index, command_pool_create_flags) }
+    }
+}
+
+impl AsRawVulkan for CommandPool {
+    type Output = vk::CommandPool;
+    fn as_raw_vulkan(&self) -> &Self::Output {
+        &self.command_pool
+    }
+}
+
+impl Drop for CommandPool {
+    fn drop(&mut self) {
+        unsafe {
+            self.device.as_raw_vulkan().destroy_command_pool(self.command_pool, None);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod new {
+        use ash::vk;
+        use jeriya_test::create_window;
+
+        use crate::{
+            command_pool::CommandPool, device::Device, entry::Entry, instance::Instance, physical_device::PhysicalDevice, surface::Surface,
+        };
+
+        #[test]
+        fn smoke() {
+            let window = create_window();
+            let entry = Entry::new().unwrap();
+            let instance = Instance::new(&entry, "my_application", false).unwrap();
+            let surface = Surface::new(&entry, &instance, &window).unwrap();
+            let physical_device = PhysicalDevice::new(&instance, &[surface]).unwrap();
+            let device = Device::new(physical_device, &instance).unwrap();
+            let _command_pool = CommandPool::new(
+                &device,
+                &device.presentation_queue,
+                vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER,
+            )
+            .unwrap();
+        }
+    }
+}
--- jeriya_backend_ash_core/src/lib.rs
+++ jeriya_backend_ash_core/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod command_pool;
 pub mod debug;
 pub mod device;
 pub mod entry;
