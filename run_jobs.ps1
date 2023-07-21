726f6bc9d0aeec6fa631bdab1ca9ae97fec750fe
Henrik Patjens <hpatjens@gmail.com>
2023-07-21 22:36:15 +0200

    not running all features in codecov

--- run_jobs.ps1
+++ run_jobs.ps1
@@ -3,7 +3,7 @@ cargo nextest run
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -Outfile codecov.exe
 
-cargo llvm-cov nextest --locked --all-features --lcov --output-path lcov.info
+cargo llvm-cov nextest --locked --lcov --output-path lcov.info
 
 .\codecov.exe -t ${CODECOV_TOKEN}
 if ($?) {
