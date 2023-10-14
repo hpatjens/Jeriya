b9860635879b3289140655f2cc3138fa4ef02ee6
Henrik Patjens <hpatjens@gmail.com>
2023-10-14 15:37:00 +0200

    also running cargo test --doc in the run_jobs script

--- run_jobs.ps1
+++ run_jobs.ps1
@@ -1,5 +1,22 @@
-cargo nextest run
+# Run cargo nextest
+$process_nextest = Start-Process -FilePath "cargo" -ArgumentList "nextest run" -NoNewWindow -Wait -PassThru
+if ($process_nextest.ExitCode -ne 0) {
+    Write-Host "cargo nextest run returned a non-zero exit code: $($process.ExitCode)" -ForegroundColor Red
+    exit
+} else {
+    Write-Host "cargo nextest run completed successfully." -ForegroundColor Green
+}
 
+# Run cargo test --doc
+$process_doctest = Start-Process -FilePath "cargo" -ArgumentList "test --doc" -NoNewWindow -Wait -PassThru
+if ($process_doctest.ExitCode -ne 0) {
+    Write-Host "cargo test --doc returned a non-zero exit code: $($process.ExitCode)" -ForegroundColor Red
+    exit
+} else {
+    Write-Host "cargo test --doc completed successfully." -ForegroundColor Green
+}
+
+# Run Codecov
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -Outfile codecov.exe
 
