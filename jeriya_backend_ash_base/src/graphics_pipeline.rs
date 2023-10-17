5d75165179d6f80f512fb66bea233adc2c7dae3d
Henrik Patjens <hpatjens@gmail.com>
2023-10-17 19:44:53 +0200

    added constant for visible meshlets

--- jeriya_backend_ash_base/src/graphics_pipeline.rs
+++ jeriya_backend_ash_base/src/graphics_pipeline.rs
@@ -188,6 +188,11 @@ where
                 .offset(0)
                 .size(std::mem::size_of::<u32>())
                 .build(),
+            vk::SpecializationMapEntry::builder()
+                .constant_id(8)
+                .offset(0)
+                .size(std::mem::size_of::<u32>())
+                .build(),
         ];
         let mut specialization_data = Vec::<u8>::new();
         specialization_data
@@ -210,6 +215,9 @@ where
             .expect("failed to write specialization constant");
         specialization_data
             .write_u32::<LittleEndian>(renderer_config.maximum_visible_rigid_mesh_instances as u32)
+            .expect("failed to write specialization constant");
+        specialization_data
+            .write_u32::<LittleEndian>(renderer_config.maximum_visible_rigid_mesh_meshlets as u32)
             .expect("failed to write specialization constant");
         let specialization_info = vk::SpecializationInfo::builder()
             .map_entries(&specialization_constants)
