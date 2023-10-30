21c77d57defdb2c9d02434921f1f0fdb3a7c2b13
Henrik Patjens <hpatjens@gmail.com>
2023-10-30 21:03:32 +0100

    updated compile shaders script

--- compile_shaders.ps1
+++ compile_shaders.ps1
@@ -1,5 +1,9 @@
-glslc jeriya_backend_ash_base\test_data\indirect_meshlet.vert -o jeriya_backend_ash_base\test_data\indirect_meshlet.vert.spv
-glslc jeriya_backend_ash_base\test_data\indirect_simple.vert -o jeriya_backend_ash_base\test_data\indirect_simple.vert.spv
-glslc jeriya_backend_ash_base\test_data\color.vert -o jeriya_backend_ash_base\test_data\color.vert.spv
-glslc jeriya_backend_ash_base\test_data\cull_rigid_mesh_instances.comp -o jeriya_backend_ash_base\test_data\cull_rigid_mesh_instances.comp.spv
-glslc jeriya_backend_ash_base\test_data\cull_rigid_mesh_meshlets.comp -o jeriya_backend_ash_base\test_data\cull_rigid_mesh_meshlets.comp.spv
+$shader_path = "jeriya_backend_ash_base\test_data\"
+$filesToProcess = Get-ChildItem -Path $shader_path -File | Where-Object { $_.Extension -ne ".spv" }
+
+foreach ($file in $filesToProcess) {  
+    $file = $file.FullName
+    $fullCommand = "glslc `"${file}`" -o `"${file}.spv`""
+    Write-Host $fullCommand -ForegroundColor Cyan
+    Invoke-Expression -Command $fullCommand
+}
