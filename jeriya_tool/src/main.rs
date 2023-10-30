874a2364175d904514daf7380f5b67c709679b9f
Henrik Patjens <hpatjens@gmail.com>
2023-10-30 13:44:11 +0100

    uniform point cloud sampling for model

--- jeriya_tool/src/main.rs
+++ jeriya_tool/src/main.rs
@@ -1,9 +1,10 @@
-use std::path::PathBuf;
+use std::{io, path::PathBuf};
 
 use clap::{Parser, ValueEnum};
 use color_eyre as ey;
 use ey::eyre::Context;
 use jeriya_content::{model::Model, point_cloud::PointCloud};
+use jeriya_shared::log::{self, info};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -30,11 +31,32 @@ enum TargetType {
 }
 
 fn main() -> ey::Result<()> {
+    // Setup logging
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                jeriya_shared::chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Trace)
+        .chain(io::stdout())
+        .apply()
+        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+
     let command_line_arguments = CommandLineArguments::parse();
     match &command_line_arguments {
         CommandLineArguments::Convert(convert) => {
+            info!("Importing model: {:?}", convert.source_filepath);
             let model = Model::import(&convert.source_filepath).wrap_err("Failed to import model")?;
-            let point_cloud = PointCloud::sample_from_model(&model, 2000.0);
+
+            info!("Converting model to point cloud");
+            let point_cloud = PointCloud::sample_from_model(&model, 0.001);
+
+            info!("Writing point cloud to OBJ");
             point_cloud
                 .to_obj(&convert.destination_filepath)
                 .wrap_err("Failed to write point cloud to OBJ")?;
