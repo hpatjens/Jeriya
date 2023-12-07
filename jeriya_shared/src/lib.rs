aadb9b2d7cd9bcd343186732c1523314311350ad
Henrik Patjens <hpatjens@gmail.com>
2023-12-07 21:34:10 +0100

    set maximum_point_cloud_pages to 8 because they caused problems in tests

--- jeriya_shared/src/lib.rs
+++ jeriya_shared/src/lib.rs
@@ -261,7 +261,7 @@ impl Default for RendererConfig {
             maximum_number_of_rigid_meshes: 2usize.pow(10),
             maximum_number_of_point_clouds: 2usize.pow(10),
             maximum_number_of_point_cloud_instances: 2usize.pow(10),
-            maximum_number_of_point_cloud_pages: 2usize.pow(14),
+            maximum_number_of_point_cloud_pages: 2usize.pow(3),
             maximum_number_of_rigid_mesh_instances: 2usize.pow(10),
             maximum_meshlets: 2usize.pow(20),
             maximum_visible_rigid_mesh_instances: 2usize.pow(10),
