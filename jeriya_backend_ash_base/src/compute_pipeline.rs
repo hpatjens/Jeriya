5e621597681c1535878050386e2669b700fdd983
Henrik Patjens <hpatjens@gmail.com>
2023-12-09 14:44:18 +0100

    added frame telemetry

--- jeriya_backend_ash_base/src/compute_pipeline.rs
+++ jeriya_backend_ash_base/src/compute_pipeline.rs
@@ -85,6 +85,7 @@ impl GenericComputePipeline {
                 .push_storage_buffer::<PointCloudPage>(24, 1)
                 .push_storage_buffer::<u32>(25, 1)
                 .push_storage_buffer::<u32>(26, 1)
+                .push_storage_buffer::<shader_interface::FrameTelemetry>(27, 1)
                 .build(device)?,
         );
         let descriptor_set_layouts = [*descriptor_set_layout.as_raw_vulkan()];
