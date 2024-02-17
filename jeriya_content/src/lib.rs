afe82c0d0f9313ec21a8bc882a29340972efd141
Henrik Patjens <hpatjens@gmail.com>
2024-02-17 21:48:58 +0100

    updated content module description

--- jeriya_content/src/lib.rs
+++ jeriya_content/src/lib.rs
@@ -5,42 +5,10 @@
 //! This crate is based around the idea of two structurally mirrored directory
 //! trees. One is the source directory, which contains the original assets
 //! and the other is the target directory, which contains the processed assets.
-//! For every file in the source directory, there is a corresponding *directory*
+//! For every file in the source directory, there is a corresponding file
 //! in the target directory which has the same name as the file. The relative
-//! location of the file in the source directory is the relative location of
-//! the directory in the target directory. Inside the directory in the target
-//! directory, there are the files containing the processed assets as well as
-//! a `asset.yaml` file which contains metadata.
-//!
-//! ## Example:
-//!
-//! **Source Directory:**
-//!
-//! ```text
-//! assets/
-//! ├─ textures/
-//! │  ├─ texture1.png
-//! │  ├─ texture2.png
-//! ├─ models/
-//! │  ├─ model1.fbx
-//! ```
-//!
-//! **Target Directory:**
-//!
-//! ```text
-//! assets/
-//! ├─ textures/
-//! │  ├─ texture1.png
-//! │  │  ├─ texture1.bin
-//! │  │  ├─ asset.yaml
-//! │  ├─ texture2.png
-//! │  │  ├─ texture2.bin
-//! │  │  ├─ asset.yaml
-//! ├─ models/
-//! │  ├─ model1.fbx
-//! │  │  ├─ model1.bin
-//! │  │  ├─ asset.yaml
-//! ```
+//! location of the file in the source directory is equal to the relative
+//! location in the target directory.
 //!
 //! # Components
 //!
